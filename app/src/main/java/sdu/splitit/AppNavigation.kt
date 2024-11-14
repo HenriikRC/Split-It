d848776b0d61b8742b271952795542cd5dcfea35
HenriikRC <hechr16@student.sdu.dk>
2024-11-14 08:59:20 +0100

    Changed from groupId to group

--- app/src/main/java/sdu/splitit/AppNavigation.kt
+++ app/src/main/java/sdu/splitit/AppNavigation.kt
@@ -1,15 +1,15 @@
 package sdu.splitit
 
-//import AddExpenseForm
 import AddExpenseForm
 import HomePageForm
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import sdu.splitit.model.Group
+import sdu.splitit.model.User
 import sdu.splitit.viewmodel.AuthViewModel
 import sdu.splitit.viewmodel.ExpenseViewModel
-//import sdu.splitit.viewmodel.ExpenseViewModel
 import sdu.splitit.viewmodel.GroupsOverviewViewModel
 import sdu.splitit.views.authentication.AddRegisterForm
 import sdu.splitit.views.authentication.LoginForm
@@ -21,7 +21,7 @@ fun AppNavigation() {
 
     NavHost(
         navController = navController,
-        startDestination = "expenseForm"
+        startDestination = "register"
     ) {
 
         composable("register") {
@@ -55,9 +55,20 @@ fun AppNavigation() {
 
 
         composable("expenseForm") {
+            val groups = mutableListOf<Group>()
+            val sampleUsers = listOf(
+
+                User(1, "Henrik", "Christensen", hashMapOf(),""),
+                User(2, "Morten", "Andersen", hashMapOf(),""),
+                User(3, "Andreas", "Honoré", hashMapOf(),""),
+                User(4, "Mathias", "Sundby", hashMapOf(),""),
+                User(5, "Lasse", "Hvilsted", hashMapOf(),""),
+                User(6, "Marcus", "Ellested", hashMapOf(),"")
+            )
+            groups.add(Group(id = 1, name = "Hackermen", members = sampleUsers, expenses = mutableListOf()))
             AddExpenseForm(
                 viewModel = ExpenseViewModel(),
-                groupId = 1,
+                group = groups[0],
                 NavHostController = navController
             )
         }
